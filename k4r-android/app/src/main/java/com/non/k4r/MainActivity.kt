fc74129046c76ffbba0e8713ea1c56a6fdbf8d78
Nondirectional <nondirectional@yeesun.com>
2024-11-26 17:38:08 +0800

    feat: ModalNavigationDrawer demo

--- k4r-android/app/src/main/java/com/non/k4r/MainActivity.kt
+++ k4r-android/app/src/main/java/com/non/k4r/MainActivity.kt
@@ -9,11 +9,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,6 +34,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.non.k4r.ui.theme.K4rTheme
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
@@ -148,4 +160,38 @@ private fun ExpenditureCardPreview() {
         )
     }
 
+}
+
+@Preview
+@Composable
+private fun TempPreview() {
+    K4rTheme {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                Column {
+                    Text("侧边栏内容1")
+                    Text("侧边栏内容1")
+                    Text("侧边栏内容1")
+                    Text("侧边栏内容1")
+                    Text("侧边栏内容1")
+                    Text("侧边栏内容1")
+                }
+            }
+        ) {
+            Scaffold(
+                topBar = {
+                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                        Icon(Icons.Default.Menu, contentDescription = "打开侧边栏")
+                    }
+                },
+                content = { innerPadding ->
+                    Text("内容区域", Modifier.padding(innerPadding))
+                }
+            )
+        }
+    }
 }
