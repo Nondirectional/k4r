9052474f05e7ce876565ca1e4c1bafb83f20e54d
nondirectional@yeesun.com <1059170150@qq.com>
2024-11-26 23:38:18 +0800

    feat: init theme

--- k4r-android/app/src/main/java/com/non/k4r/MainActivity.kt
+++ k4r-android/app/src/main/java/com/non/k4r/MainActivity.kt
@@ -4,36 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.non.k4r.ui.theme.K4rTheme
+import com.non.k4r.ui.theme.AppTheme
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -45,11 +56,81 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            K4rTheme {
+            MainScreen()
+        }
+
+    }
+
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    AppTheme {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                Surface(modifier= Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    Column(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .fillMaxHeight()
+                            .width(200.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    ) {
+                        Image(
+                            painter = painterResource(R.mipmap.default_avatar),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillBounds,
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(CircleShape)
+                        )
+                        Spacer(Modifier.height(8.dp))
+                        Text(color = MaterialTheme.colorScheme.onSurface, text = "未登录")
+
+                    }
+                }
+
+            }
+        ) {
+            Scaffold(
+                topBar = {
+                    Surface(
+                        modifier = Modifier.safeDrawingPadding()
+                    ) {
+                        IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                            Icon(Icons.Default.Menu, contentDescription = "打开侧边栏")
+                        }
+                    }
+                },
+                content = { innerPadding ->
+                    TimelineScreen(
+                        modifier.padding(
+                            top = innerPadding.calculateTopPadding(),
+                            bottom = innerPadding.calculateBottomPadding()
+                        )
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun TimelineScreen(modifier: Modifier) {
+    Surface(modifier = modifier) {
+        val list = (1..100).toList()
+        LazyColumn {
+            items(list) { number ->
+                Text("$number")
             }
         }
     }
 }
+
 
 @Composable
 fun Card(
@@ -71,7 +152,7 @@ fun ExpenditureCard(
     tags: List<String>,
     detail: String
 ) {
-    K4rTheme {
+    AppTheme() {
         Surface(
             shape = MaterialTheme.shapes.medium,
             modifier = modifier
@@ -160,38 +241,4 @@ private fun ExpenditureCardPreview() {
         )
     }
 
-}
-
-@Preview
-@Composable
-private fun TempPreview() {
-    K4rTheme {
-        val drawerState = rememberDrawerState(DrawerValue.Closed)
-        val scope = rememberCoroutineScope()
-
-        ModalNavigationDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-                Column {
-                    Text("侧边栏内容1")
-                    Text("侧边栏内容1")
-                    Text("侧边栏内容1")
-                    Text("侧边栏内容1")
-                    Text("侧边栏内容1")
-                    Text("侧边栏内容1")
-                }
-            }
-        ) {
-            Scaffold(
-                topBar = {
-                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
-                        Icon(Icons.Default.Menu, contentDescription = "打开侧边栏")
-                    }
-                },
-                content = { innerPadding ->
-                    Text("内容区域", Modifier.padding(innerPadding))
-                }
-            )
-        }
-    }
 }
